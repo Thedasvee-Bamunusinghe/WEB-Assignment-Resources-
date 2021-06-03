0e29ddcb99f653a75a9b554d890fd8b223b4e3c4
Thedasvee Bamunusinghe <84556539+Thedasvee-Bamunusinghe@users.noreply.github.com>
2021-06-03 10:42:58 +0530

    Applicant Info
    Current form which will be used to input database values

---
+++ Applicant Info.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Application Form</title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+<form name="details" method="post">
+	<table>
+		<h1> Enter Your Details </h1>
+		<tr><td>First Name:</td>
+			<td><input type="text" name="Fname"></td>
+		</tr>
+		<tr><td>Last Name:</td>
+			<td><input type="text" name="Lname"></td>
+		</tr>
+		<tr><td>Email:</td>
+			<td><input type="email" name="Fname"></td>
+		</tr>
+		<tr><td></td>
+			<td><input type="submit" name="done" value="Submit"></td>
+		</tr>
+		
+</table>
+</form>
+
+
+</body>
+</html>

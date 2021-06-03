76f7a31dc37484beb0f25c966def1a17eddd12b4
AkilaWeragoda <84671621+AkilaWeragoda@users.noreply.github.com>
2021-06-04 02:49:12 +0530

    Update Applicant Info Inner.php

--- Applicant Info Inner.php
+++ Applicant Info Inner.php
@@ -1,9 +1,10 @@
 <?php
-
+/* These super global variables will grab the information input to the form and assign them to a local variable*/
 $fname= $_POST["Fname"];
 $lname=$_POST["Lname"];
 $email=$_POST["email"];
 
+/* This is a redirect function, if fields are empty it shall go back to the form page */
 if($fname=="")
 {
 	echo '<script>

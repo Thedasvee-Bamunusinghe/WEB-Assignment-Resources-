c8b1ed5f62cc3c2d2dcfd2647a8410b1ff70e743
AkilaWeragoda <84671621+AkilaWeragoda@users.noreply.github.com>
2021-06-10 12:36:59 +0530

    Final Update

--- Form.php
+++ Form.php
@@ -32,22 +32,26 @@
 	<div class="form-group">
 	<!--i tags use the Fontawesome library to show various icons on the page, here it is used to show a user icon -->
 	<label><i class="fa fa-user" aria-hidden="true"></i> First Name:</label><br>
+		<!--Input box to recieve first name -->
 		<input type="text" name="Fname">
 	</div>
 	
 	<div class="form-group">
 		<!--Fontawesome icon for a comment -->
 		<label><i class="fa fa-comment" aria-hidden="true"></i> Last Name:</label><br>
+		<!--Input box to recieve last name -->
 		<input type="text" name="Lname">
 	</div>
 	
 	<div class="form-group">
 		<!--Fontawesome icon for an envelope -->
 		<label><i class="fa fa-envelope" aria-hidden="true"></i> Email:</label><br>
+		<!--Input box to recieve email-->
 		<input type="email" name="email"><br>
 	</div>
 	
 	<div class="form-group">
+		<!--Button used to confirm information, it will send the information to the php file through the post method -->
 		<input type="submit" name="done" value="Submit">
 	</div>	
 		
